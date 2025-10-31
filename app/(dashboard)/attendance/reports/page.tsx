5cacd358cb7180395c5cbd697194a9701498bc0f
Tarun Vadde <tarun@soulverse.us>
2025-10-31 22:40:44 +0530

    chore: attendance update
    Signed-off-by: Tarun Vadde <tarun@soulverse.us>

--- app/(dashboard)/attendance/reports/page.tsx
+++ app/(dashboard)/attendance/reports/page.tsx
@@ -1,9 +1,5 @@
 import { AttendanceReportsComponent } from "@/components/attendance/attendance-reports"
 
 export default function AttendanceReportsPage() {
-  return (
-    <div className="container mx-auto py-6">
-        <AttendanceReportsComponent />
-    </div>
-  )
+  return <AttendanceReportsComponent />
 }

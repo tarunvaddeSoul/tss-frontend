f456258f956aaaae62cc65ffc0f6dce96fba8691
Tarun Vadde <tarun@soulverse.us>
2025-10-31 22:40:22 +0530

    chore: attendance update
    Signed-off-by: Tarun Vadde <tarun@soulverse.us>

--- app/(dashboard)/attendance/mark-by-site/page.tsx
+++ app/(dashboard)/attendance/mark-by-site/page.tsx
@@ -3,9 +3,5 @@
 import { MarkAttendanceBySite } from "@/components/attendance/mark-attendance-by-site"
 
 export default function MarkAttendanceBySitePage() {
-  return (
-    <div className="container mx-auto py-6">
-      <MarkAttendanceBySite />
-    </div>
-  )
+  return <MarkAttendanceBySite />
 }
